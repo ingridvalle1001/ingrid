b07f9a7ffffab0f145bb146a1d58db0807639126
ingridvalle1001 <75874231+ingridvalle1001@users.noreply.github.com>
2021-03-08 21:02:09 -0800

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+<script src="/js/scripts.js"></script>
+
+  <script src="https://cdn.rawgit.com/sachinchoolur/lightgallery.js/master/dist/js/lightgallery.js"></script>
+
+  <script type="text/javascript">
+    lightgallery(document.getElementById('portfoliogallery'));
+  </script>
